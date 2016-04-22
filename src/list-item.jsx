85a21cb1034bf58733ca405fce0a4158398a0f88
johel <johel.carvalho@gmail.com>
2016-04-22 14:46:07 -0300

    updating done property to firebase

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -1,20 +1,37 @@
 var React = require('react');
+var Firebase = require('firebase');
+var rootUrl = 'https://shining-heat-7015.firebaseio.com/';
 
 module.exports = React.createClass({
 	getInitialState:function(){
 		return {
-			text:this.props.item.text
+			text:this.props.item.text,
+			done:this.props.item.done
 		};
+	},
+	componentWillMount:function(){
+		this.itemRef = new Firebase(rootUrl + 'items/' + this.props.item.key);
+		// console.log('itemRef', this.props.item.key);
 	},
 	render: function(){
 		return  <div className="input-group">
       <span className="input-group-addon">
-        <input type="checkbox" />
+        <input onChange={this.onDoneChange}  type="checkbox" />
       </span>
-      <input value={this.state.text} type="text" className="form-control" />
+      <input onChange={this.onTextChange} value={this.state.text} type="text" className="form-control" />
       <span className="input-group-btn">
         <button className="btn btn-danger" type="button">Delete</button>
       </span>
     </div>
+	},
+	onTextChange:function(event){
+		this.setState({
+			text:event.target.value
+		});
+	},
+	onDoneChange:function(event){
+		var update = {done:event.target.checked};
+		this.setState(update);
+		this.itemRef.update(update);
 	}
 })
--- src/list.jsx
+++ src/list.jsx
@@ -8,10 +8,8 @@ module.exports = React.createClass({
 		</div>
 	},
 	renderListItems:function(){
-		console.log('itens na lista', this.props.items);
 		var keysArray = Object.keys(this.props.items);
 		var result = [];
-		console.log(keysArray);
 		if(this.props.items && keysArray.length === 0){
 			return <li>Add a todo to get started</li>
 		}else{
@@ -19,17 +17,8 @@ module.exports = React.createClass({
 			keysArray.forEach(function(key, index){
 				var item = this.props.items[key];
 				item.key = key;
-				// console.log('item', item);
 				result.push(<ListItem key={index} item={item} />)
 			}.bind(this));
-			
-			// for(var key in this.props.items){
-			// 	result.push(
-			// 		<li>
-			// 			{this.props.items[key].text}
-			// 		</li>
-			// 	)
-			// }
 
 			return result;
 		}
